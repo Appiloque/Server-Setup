92333f72ff1c29a5fcf9b5dd2f73cb615f12740d
xiutiing <57886844+xiutiing@users.noreply.github.com>
2020-08-27 16:36:35 +0800

    Delete login.sh

--- login.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#wp-config
-echo START MODIFYING WP-CONFIG
-sed  -i "/That's all, stop editing! Happy publishing./i//CHANGE LOGIN SLUG" /opt/easyengine/sites/$SITE/app/wp-config.php
-sed  -i "/That's all, stop editing! Happy publishing./idefine('WP_ADMIN_DIR', 'admin');" /opt/easyengine/sites/$SITE/app/wp-config.php
-sed  -i "/That's all, stop editing! Happy publishing./idefine('ADMIN_COOKIE_PATH', SITECOOKIEPATH . WP_ADMIN_DIR);" /opt/easyengine/sites/$SITE/app/wp-config.php
-echo MODIFYING WP-CONFIG SUCCESSFULLY
